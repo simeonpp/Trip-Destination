15aae423615a1c5722ad506f89eb89489590f8a1
Your Name <you@example.com>
2016-02-08 21:26:39 +0200

    Added Car model.

--- Source/TripDestination/Common/TripDestination.Common.Infrastructure/Constants/ModelConstants.cs
+++ Source/TripDestination/Common/TripDestination.Common.Infrastructure/Constants/ModelConstants.cs
@@ -13,5 +13,24 @@
 
         public const int UserDescriptionMinLength = 20;
         public const int UserDescriptionMaxLength = 1000;
+
+        // Car
+        public const int CarTotalSeatsMinLength = 1;
+        public const int CarTotalSeatsMaxLength = 5;
+
+        public const int CarBrandMinLength = 3;
+        public const int CarBrandMaxLength = 50;
+
+        public const int CarModelMinLength = 2;
+        public const int CarModelMaxLength = 50;
+
+        public const int CarColorMinLength = 3;
+        public const int CarColorMaxLength = 50;
+
+        public const int CarYearMinYear = 1900;
+        public const int CarYearMaxYear = 2016;
+
+        public const int CarDescriptionMinLength = 20;
+        public const int CarDescriptionMaxLength = 1000;
     }
 }
