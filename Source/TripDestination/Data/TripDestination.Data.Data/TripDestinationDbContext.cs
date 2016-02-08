854dd8cdabd36d633ccf44a38dde1427092ab66d
Your Name <you@example.com>
2016-02-08 21:36:44 +0200

    Added models to the DbContext.

--- Source/TripDestination/Data/TripDestination.Data.Data/TripDestinationDbContext.cs
+++ Source/TripDestination/Data/TripDestination.Data.Data/TripDestinationDbContext.cs
@@ -2,9 +2,8 @@
 {
     using Microsoft.AspNet.Identity.EntityFramework;
     using Models;
-    using System.Collections.Generic;
     using System.Data.Entity;
-    using System.Data.Entity.ModelConfiguration.Conventions;
+
     public class TripDestinationDbContext : IdentityDbContext<User>
     {
         public TripDestinationDbContext()
@@ -16,5 +15,46 @@
         {
             return new TripDestinationDbContext();
         }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Trip>()
+                        .HasRequired(m => m.From)
+                        .WithMany(t => t.FromTrips)
+                        .HasForeignKey(m => m.FromId)
+                        .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Trip>()
+                        .HasRequired(m => m.To)
+                        .WithMany(t => t.ToTrips)
+                        .HasForeignKey(m => m.ToId)
+                        .WillCascadeOnDelete(false);
+        }
+
+        public virtual IDbSet<Car> Cars { get; set; }
+
+        public virtual IDbSet<Comment> Comments { get; set; }
+
+        public virtual IDbSet<Like> Likes { get; set; }
+
+        public virtual IDbSet<Newsletter> Newsletters { get; set; }
+
+        public virtual IDbSet<Notification> Notifications { get; set; }
+
+        public virtual IDbSet<NotificationType> NotificationTypes { get; set; }
+
+        public virtual IDbSet<PassengerTrip> PassengerTrips { get; set; }
+
+        public virtual IDbSet<Photo> Photos { get; set; }
+
+        public virtual IDbSet<Rating> Ratings { get; set; }
+
+        public virtual IDbSet<Town> Towns { get; set; }
+
+        public virtual IDbSet<Trip> Trips { get; set; }
+
+        public virtual IDbSet<View> Views { get; set; }
     }
 }
