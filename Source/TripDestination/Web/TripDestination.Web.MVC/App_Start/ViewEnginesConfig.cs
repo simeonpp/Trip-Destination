45df26feacf8870e7fc79e50f1941cd9d7a4b66c
Your Name <you@example.com>
2016-02-09 08:34:01 +0200

    Added ViewEnginesConfig.cs.

---
+++ Source/TripDestination/Web/TripDestination.Web.MVC/App_Start/ViewEnginesConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace TripDestination.Web.MVC
+{
+    using System.Web.Mvc;
+
+    public static class ViewEnginesConfig
+    {
+        public static void Register(ViewEngineCollection Engines)
+        {
+            Engines.Clear();
+            Engines.Add(new RazorViewEngine());
+        }
+    }
+}
