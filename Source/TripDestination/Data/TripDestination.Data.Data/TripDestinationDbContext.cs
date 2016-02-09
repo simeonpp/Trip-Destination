fc94b5b31882e33db669d26a5c194e5bbf84dddf
Your Name <you@example.com>
2016-02-09 17:41:23 +0200

    Added Repository pattern.

--- Source/TripDestination/Data/TripDestination.Data.Data/TripDestinationDbContext.cs
+++ Source/TripDestination/Data/TripDestination.Data.Data/TripDestinationDbContext.cs
@@ -5,7 +5,7 @@
     using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.ModelConfiguration.Conventions;
-    public class TripDestinationDbContext : IdentityDbContext<User>
+    public class TripDestinationDbContext : IdentityDbContext<User>, ITripDestinationDbContext
     {
         public TripDestinationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
