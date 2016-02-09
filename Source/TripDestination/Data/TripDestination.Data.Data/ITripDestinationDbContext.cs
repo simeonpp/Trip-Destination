c4542606a5c404e95788951e5496d570ea10c80f
Simeon Petkov <you@example.com>
2016-02-09 19:43:36 +0200

    Added Db Set to the IDbContext Interface.

--- Source/TripDestination/Data/TripDestination.Data.Data/ITripDestinationDbContext.cs
+++ Source/TripDestination/Data/TripDestination.Data.Data/ITripDestinationDbContext.cs
@@ -9,7 +9,35 @@
     {
         int SaveChanges();
 
-        //IDbSet<User> Users { get; set; }
+        IDbSet<User> Users { get; set; }
+
+        IDbSet<Car> Cars { get; set; }
+
+        IDbSet<Comment> Comments { get; set; }
+
+        IDbSet<Like> Likes { get; set; }
+
+        IDbSet<Newsletter> Newsletters { get; set; }
+
+        IDbSet<Notification> Notifications { get; set; }
+
+        IDbSet<NotificationType> NotificationTypes { get; set; }
+
+        IDbSet<PassengerTrip> PassengerTrips { get; set; }
+
+        IDbSet<Photo> Photos { get; set; }
+
+        IDbSet<Rating> Ratings { get; set; }
+
+        IDbSet<Town> Towns { get; set; }
+
+        IDbSet<Trip> Trips { get; set; }
+
+        IDbSet<View> Views { get; set; }
+
+        IDbSet<Page> Pages { get; set; }
+
+        IDbSet<PageParagraph> PageParagraphs { get; set; }
 
         DbSet<TEntity> Set<TEntity>() where TEntity : class;
 
