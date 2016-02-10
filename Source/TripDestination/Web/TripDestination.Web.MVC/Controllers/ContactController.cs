9b75cdbcd06d759171f6c46efc799bdb6d9a41bb
Simeon Petkov <you@example.com>
2016-02-10 07:53:56 +0200

    Added Contact page.

---
+++ Source/TripDestination/Web/TripDestination.Web.MVC/Controllers/ContactController.cs
@@ -0,0 +1,19 @@
+﻿namespace TripDestination.Web.MVC.Controllers
+{
+    using System.Web.Mvc;
+
+    public class ContactController : BaseController
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public ActionResult ContactForm()
+        {
+            return this.RedirectToAction("Index");
+        }
+    }
+}
---
+++ Source/TripDestination/Web/TripDestination.Web.MVC/ViewModels/Contact/ContactFormViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TripDestination.Web.MVC.ViewModels.Contact
+{
+    public class ContactFormViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
