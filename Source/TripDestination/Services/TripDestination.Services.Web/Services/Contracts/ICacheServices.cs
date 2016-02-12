cf426a501336e84b7f9dd518ea5c621642f8630e
Simeon Petkov <you@example.com>
2016-02-12 20:18:32 +0200

    Implemented HttpCacheServices.

---
+++ Source/TripDestination/Services/TripDestination.Services.Web/Services/Contracts/ICacheServices.cs
@@ -0,0 +1,11 @@
+﻿namespace TripDestination.Services.Web.Services.Contracts
+{
+    using System;
+
+    public interface ICacheServices
+    {
+        T Get<T>(string itemName, Func<T> getDataFunc, int durationInSeconds);
+
+        void Remove(string itemName);
+    }
+}
--- Source/TripDestination/Web/TripDestination.Web.MVC/Controllers/AccountController.cs
+++ Source/TripDestination/Web/TripDestination.Web.MVC/Controllers/AccountController.cs
@@ -281,7 +281,7 @@ namespace TripDestination.Web.MVC.Controllers
         public ActionResult ExternalLogin(string provider, string returnUrl)
         {
             // Request a redirect to the external login provider
-            return new ChallengeResult(provider, this.sUrl.Action("ExternalLoginCallback", "Account", new { ReturnUrl = returnUrl }));
+            return new ChallengeResult(provider, this.Url.Action("ExternalLoginCallback", "Account", new { ReturnUrl = returnUrl }));
         }
 
         // GET: /Account/SendCode
