ce6c6d722fa2361b8d189949bcb8d7c0fbdad04b
Simeon Petkov <you@example.com>
2016-02-12 10:44:35 +0200

    Added NUnit test project for Services.Web + test for IIdentifierProvider

---
+++ Source/TripDestination/TripDestination.Tests.Services.Web/IdentifierProviderTests.cs
@@ -0,0 +1,21 @@
+﻿namespace TripDestination.Tests.Services.Web
+{
+    using NUnit.Framework;
+    using TripDestination.Services.Web.Providers;
+    using TripDestination.Services.Web.Providers.Contracts;
+
+    [TestFixture]
+    public class IdentifierProviderTests
+    {
+        [Test]
+        public void EncodingAndDecodingShouldWorkCorrectly()
+        {
+            const int id = 56;
+
+            IIdentifierProvider identProvider = new IdentifierProvider();
+            var endodedId = identProvider.EncodeId(id);
+            var actual = identProvider.DecodeId(endodedId);
+            Assert.AreEqual(id, actual);
+        }
+    }
+}
