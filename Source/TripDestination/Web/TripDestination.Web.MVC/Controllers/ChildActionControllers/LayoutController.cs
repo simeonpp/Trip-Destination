26b4c889b5eb292c0ce2fedf58e359f593b16bb5
Simeon Petkov <you@example.com>
2016-02-14 17:14:26 +0200

    Added footer partial.

---
+++ Source/TripDestination/Web/TripDestination.Web.MVC/Controllers/ChildActionControllers/LayoutController.cs
@@ -0,0 +1,13 @@
+﻿namespace TripDestination.Web.MVC.Controllers.ChildActionControllers
+{
+    using System.Web.Mvc;
+
+    public class LayoutController : Controller
+    {
+        [ChildActionOnly]
+        public ActionResult FooterPartial()
+        {
+            return this.PartialView("~/Views/Shared/_FooterPartial.cshtml");
+        }
+    }
+}
