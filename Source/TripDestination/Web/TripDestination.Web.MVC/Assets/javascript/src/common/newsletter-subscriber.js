467959e1e6bd0b5811034c5318e38a49a538a98c
Simeon Petkov <you@example.com>
2016-02-15 12:11:12 +0200

    Finish logic for newsletter subscribe - js.

--- Source/TripDestination/Web/TripDestination.Web.MVC/Assets/javascript/src/common/newsletter-subscriber.js
+++ Source/TripDestination/Web/TripDestination.Web.MVC/Assets/javascript/src/common/newsletter-subscriber.js
@@ -3,10 +3,23 @@
 
     $newsletterButton.on('click', function () {
         var $newsletterEmailInput = $('#newsletterEmailInput'),
-            emailPattern = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+            emailPattern = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i,
+            email = $newsletterEmailInput.val();
 
-        if (emailPattern.test($newsletterEmailInput.val())) {
-            toastr.success('You have successfully subscribed to our newsletter.');
+        if (emailPattern.test(email)) {
+            $.getJSON(
+                '/Newsletter/Subscribe',
+                {
+                    email: email
+                },
+                function (response) {
+                    if (reseponse.status) {
+                        toastr.success('You have successfully subscribed email ' + response.data + ' to our newsletter.');
+                    } else {
+                        toastr.error('An error has occured during trying to register to newsletter. Please try again or contact our team for help.');
+                    }
+                }
+            )
         } else {
             toastr.error('You have entered invalid email.');
         }
