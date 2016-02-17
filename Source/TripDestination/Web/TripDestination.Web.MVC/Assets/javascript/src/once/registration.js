43d43e2558e301bf7b9b368f8681878cd18c3466
Simeon Petkov <you@example.com>
2016-02-17 23:17:50 +0200

    Added js to registration form to show/hide form and some mark up and viewmodel tunings.

--- Source/TripDestination/Web/TripDestination.Web.MVC/Assets/javascript/src/once/registration.js
+++ Source/TripDestination/Web/TripDestination.Web.MVC/Assets/javascript/src/once/registration.js
@@ -1,1 +1,27 @@
-﻿
+﻿$().ready(function () {
+    var $selectRole = $('#Role'),
+        $driverAdditionalWrap = $('#driverAdditionalWrap'),
+        adnimationShowSpeed = 250;
+
+    initCheck()
+
+    $selectRole.on('change', function () {
+        var $this = $(this),
+            selectedValue = $this.val();
+
+        toogleDriverAdditinalWrap(selectedValue);
+    })
+
+    function toogleDriverAdditinalWrap(value) {
+        if (value == "Driver") {
+            $driverAdditionalWrap.show(adnimationShowSpeed);
+        } else {
+            $driverAdditionalWrap.hide(adnimationShowSpeed);
+        }
+    }
+
+    function initCheck() {
+        var initialValue = $("#Role option:selected").val();
+        toogleDriverAdditinalWrap(initialValue);
+    }
+});
