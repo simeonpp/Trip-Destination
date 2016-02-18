c0f4a7c447f99ea02c4898c47c6cfd623fd60376
Simeon Petkov <you@example.com>
2016-02-18 11:40:47 +0200

    Added cookie policy Controller and View.

---
+++ Source/TripDestination/Web/TripDestination.Web.MVC/Controllers/CookiePolicyController.cs
@@ -0,0 +1,12 @@
+﻿namespace TripDestination.Web.MVC.Controllers
+{
+    using System.Web.Mvc;
+
+    public class CookiePolicyController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
