cea36e8fa7ad0541fc114d4aa486634199858ffa
Simeon Petkov <you@example.com>
2016-02-20 21:43:53 +0200

    Added Kendo scafolding for cars admin.

---
+++ Source/TripDestination/Web/TripDestination.Web.MVC/Areas/Admin/Controllers/CarAdminController.cs
@@ -0,0 +1,117 @@
+﻿namespace TripDestination.Web.MVC.Areas.Admin.Controllers
+{
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Web.Mvc;
+    using Kendo.Mvc.Extensions;
+    using Kendo.Mvc.UI;
+    using TripDestination.Data.Models;
+    using TripDestination.Data.Data;
+
+    public class CarAdminController : Controller
+    {
+        private TripDestinationDbContext db = new TripDestinationDbContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cars_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            IQueryable<Car> cars = db.Cars;
+            DataSourceResult result = cars.ToDataSourceResult(request, c => new CarAdminViewModel 
+            {
+                TotalSeats = c.TotalSeats,
+                Brand = c.Brand,
+                Model = c.Model,
+                Color = c.Color,
+                Year = c.Year,
+                SpaceForLugage = c.SpaceForLugage,
+                Description = c.Description
+            });
+
+            return Json(result);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Cars_Create([DataSourceRequest]DataSourceRequest request, CarAdminViewModel car)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new Car
+                {
+                    TotalSeats = car.TotalSeats,
+                    Brand = car.Brand,
+                    Model = car.Model,
+                    Color = car.Color,
+                    Year = car.Year,
+                    SpaceForLugage = car.SpaceForLugage,
+                    Description = car.Description
+                };
+
+                db.Cars.Add(entity);
+                db.SaveChanges();
+                car.Id = entity.Id;
+            }
+
+            return Json(new[] { car }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Cars_Update([DataSourceRequest]DataSourceRequest request, CarAdminViewModel car)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new Car
+                {
+                    Id = car.Id,
+                    TotalSeats = car.TotalSeats,
+                    Brand = car.Brand,
+                    Model = car.Model,
+                    Color = car.Color,
+                    Year = car.Year,
+                    SpaceForLugage = car.SpaceForLugage,
+                    Description = car.Description
+                };
+
+                db.Cars.Attach(entity);
+                db.Entry(entity).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            return Json(new[] { car }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Cars_Destroy([DataSourceRequest]DataSourceRequest request, CarAdminViewModel car)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new Car
+                {
+                    Id = car.Id,
+                    TotalSeats = car.TotalSeats,
+                    Brand = car.Brand,
+                    Model = car.Model,
+                    Color = car.Color,
+                    Year = car.Year,
+                    SpaceForLugage = car.SpaceForLugage,
+                    Description = car.Description
+                };
+
+                db.Cars.Attach(entity);
+                db.Cars.Remove(entity);
+                db.SaveChanges();
+            }
+
+            return Json(new[] { car }.ToDataSourceResult(request, ModelState));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Source/TripDestination/Web/TripDestination.Web.MVC/Areas/Admin/ViewModels/CarAdminViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace TripDestination.Web.MVC.Areas.Admin.ViewModels
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+    using Common.Infrastructure.Constants;
+    using TripDestination.Common.Infrastructure.Mapping;
+    using TripDestination.Data.Models;
+
+    public class CarAdminViewModel : IMapFrom<Car>
+    {
+        [Required]
+        [Range(ModelConstants.CarTotalSeatsMinLength, ModelConstants.CarTotalSeatsMaxLength, ErrorMessage = "Car total seat must be between 1 and 5.")]
+        public int TotalSeats { get; set; }
+
+        [Required]
+        [MinLength(ModelConstants.CarBrandMinLength, ErrorMessage = "Car brand can no be less than 3 symbols long.")]
+        [MaxLength(ModelConstants.CarBrandMaxLength, ErrorMessage = "Car brand can no be more than 50 symbols long.")]
+        public string Brand { get; set; }
+
+        [Required]
+        [MinLength(ModelConstants.CarModelMinLength, ErrorMessage = "Car model can no be less than 3 symbols long.")]
+        [MaxLength(ModelConstants.CarModelMaxLength, ErrorMessage = "Car model can no be more than 50 symbols long.")]
+        public string Model { get; set; }
+
+        [Required]
+        [MinLength(ModelConstants.CarColorMinLength, ErrorMessage = "Car color can no be less than 3 symbols long.")]
+        [MaxLength(ModelConstants.CarColorMaxLength, ErrorMessage = "Car color can no be more than 50 symbols long.")]
+        public string Color { get; set; }
+
+        [Range(ModelConstants.CarYearMinYear, ModelConstants.CarYearMaxYear, ErrorMessage = "Car year is not in range.")]
+        public int? Year { get; set; }
+
+        [Required]
+        public SpaceForLugage SpaceForLugage { get; set; }
+
+        [AllowHtml]
+        [MinLength(ModelConstants.CarDescriptionMinLength, ErrorMessage = "Car description can no be less than 20 symbols long.")]
+        [MaxLength(ModelConstants.CarDescriptionMaxLength, ErrorMessage = "Car description can no be more than 1000 symbols long.")]
+        public string Description { get; set; }
+    }
+}
