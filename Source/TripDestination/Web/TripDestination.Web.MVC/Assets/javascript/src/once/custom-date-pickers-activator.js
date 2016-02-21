f8fffc8369beb5c47ec717d436af7000a7894f61
Simeon Petkov <you@example.com>
2016-02-21 08:09:17 +0200

    Fix datetime picker on trip list page.

--- Source/TripDestination/Web/TripDestination.Web.MVC/Assets/javascript/src/once/custom-date-pickers-activator.js
+++ Source/TripDestination/Web/TripDestination.Web.MVC/Assets/javascript/src/once/custom-date-pickers-activator.js
@@ -1,6 +1,7 @@
 ﻿$().ready(function () {
     $('.customDateTimePickers').datetimepicker({
         format: 'DD MMMM YYYY',
+        defaultDate: new Date(),
         icons: {
             previous: 'glyphicon glyphicon-arrow-left',
             next: 'glyphicon glyphicon-arrow-right'
--- Source/TripDestination/Web/TripDestination.Web.MVC/Assets/javascript/src/once/custom-datetime-pickers-activator.js
+++ Source/TripDestination/Web/TripDestination.Web.MVC/Assets/javascript/src/once/custom-datetime-pickers-activator.js
@@ -1,6 +1,7 @@
 ﻿$().ready(function () {
     $('.customDateTimePickers').datetimepicker({
         format: 'DD MMMM YYYY LT',
+        defaultDate: new Date(),
         icons: {
             previous: 'glyphicon glyphicon-arrow-left',
             next: 'glyphicon glyphicon-arrow-right'
