ff2d070d6493e36f20a499cfe49fa2cb3f1b9fc6
Simeon Petkov <you@example.com>
2016-02-22 11:26:12 +0200

    Added Trip controller route tests.

--- Source/TripDestination/TripDestination.Test.Routes/TripRouteTests.cs
+++ Source/TripDestination/TripDestination.Test.Routes/TripRouteTests.cs
@@ -7,12 +7,12 @@
     using Web.MVC;
 
     [TestFixture]
-    public class TripDetailsRouteTests
+    public class TripRouteTests
     {
         private const int pageId = 558;
 
         [Test]
-        public void TripRouteByIdShouldWorkCorrectlyWithoutSlug()
+        public void TripRouteDetailsShouldWorkCorrectlyWithoutSlug()
         {
             string Url = "/Trip/" + pageId;
             var routeCollection = new RouteCollection();
@@ -21,13 +21,25 @@
         }
 
         [Test]
-        public void TripRouteByIdShouldWorkCorrectlyWithSlug()
+        public void TripRouteDetailsShouldWorkCorrectlyWithSlug()
         {
             string randomSlug = "Sofia-Burgas";
             string Url = "/Trip/" + pageId + "/" + randomSlug;
             var routeCollection = new RouteCollection();
             RouteConfig.RegisterRoutes(routeCollection);
             routeCollection.ShouldMap(Url).To<TripController>(c => c.Details(pageId));
+        }
+
+        [Test]
+        public void TripRouteListShouldWorkCorrectly()
+        {
+            string calendarDate = "2016-01-01";
+            int page = 1;
+
+            string Url = "/Trip/List?calendarDate=" + calendarDate + "&page=" + page;
+            var routeCollection = new RouteCollection();
+            RouteConfig.RegisterRoutes(routeCollection);
+            routeCollection.ShouldMap(Url).To<TripController>(c => c.List(calendarDate, page));
         }
     }
 }
