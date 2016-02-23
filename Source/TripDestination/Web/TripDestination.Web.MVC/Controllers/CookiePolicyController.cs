c9a9a5360c24323e2d6c356e0b2d94796cf4fe0c
Simeon Petkov <you@example.com>
2016-02-23 07:15:13 +0200

    Implement cookie policy view/hide logic

--- Source/TripDestination/Web/TripDestination.Web.MVC/Controllers/CookiePolicyController.cs
+++ Source/TripDestination/Web/TripDestination.Web.MVC/Controllers/CookiePolicyController.cs
@@ -1,11 +1,13 @@
 ﻿namespace TripDestination.Web.MVC.Controllers
 {
+    using System.Web;
     using System.Web.Mvc;
 
     public class CookiePolicyController : BaseController
     {
         public ActionResult Index()
         {
+            this.Response.SetCookie(new HttpCookie("cookiePolicySeen", "true"));
             return this.View();
         }
     }
